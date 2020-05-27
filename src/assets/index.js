dd270b3ce822c2872058c1c662511fb026c656a5
Bhupendra <bhupendra1011@gmail.com>
2020-05-27 10:48:56 +0530

    adding theme options

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import SignUp from "./illustrations/signUp.svg";
+
+export const Illustrations = {
+  SignUp,
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
-import PrimaryButton, {
+import { ThemeProvider } from "styled-components";
+
+import {
+  PrimaryButton,
   SecondaryButton,
   TertiaryButton,
 } from "./components/Buttons";
 
-const App = () => (
-  <>
-    <PrimaryButton>Hello World !</PrimaryButton>
-    <SecondaryButton>Hello World !</SecondaryButton>
-    <TertiaryButton>Hello World !</TertiaryButton>
-  </>
-);
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
+
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{
+          margin: "0 16px 24px",
+          padding: "8px",
+          background: "none",
+          cursor: "pointer",
+          border: "2px solid #000",
+          marginTop: "60px",
+        }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark Theme
+      </button>
+      <button
+        style={{
+          margin: "0 16px 24px",
+          padding: "8px",
+          background: "none",
+          cursor: "pointer",
+          border: "2px solid #000",
+          marginTop: "60px",
+        }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default Theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <PrimaryButton>Hello World !</PrimaryButton>
+        <SecondaryButton>Hello World !</SecondaryButton>
+        <TertiaryButton>Hello World !</TertiaryButton>
+      </div>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
 ReactDom.render(<App />, document.querySelector("#root"));
